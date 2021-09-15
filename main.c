c721d848e5eadb953ec0088cdcfe26907aad041a
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-09-15 16:54:18 +0200

    Fixed includes, and also now uses stdbool

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
-#define TRUE 1
-#include "stdlib.h"
-#include "stdio.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 		
-	while(TRUE){
-	type_prompt();
-	read_command(command, parameters);
+	while(true){
+		type_prompt();
+		read_command(command, parameters);
 
-	if(fork() != 0){
-	  /* Parent code */
-		waitpid(-1, &status, 0);
-	} else {
-		/* Child code*/
-		execve(command, parameters, 0);
+		if(fork() != 0){
+		  /* Parent code */
+			waitpid(-1, &status, 0);
+		} else {
+			/* Child code*/
+			execve(command, parameters, 0);
 
-	}
-	
+		}
 	}
 return 0;
 }
