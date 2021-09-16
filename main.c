558785130c87c453d63568c48b905b01a467bb85
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-09-16 10:01:17 +0200

    Fixed nonsensical whitespace and indentation

--- main.c
+++ main.c
@@ -3,38 +3,29 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-
-
-
 int main(){
-
 	while(true){
-	
-	// read command from prompt
-	//char inputStr[100];  
-	//scanf("%s", inputStr);
-        
-	// read_command(command, parameters);
-			
-	int childPid = fork(); 
-	
-	if(childPid > 0)
-	{
-		printf("Error!");
-		return 0;
-	}
+		// read command from prompt
+		//char inputStr[100];
+		//scanf("%s", inputStr);
 
-	if(childPid != 0)
-	{
-		//waitpid(-1, &status, 0);
-		printf("I am a parent with PID: %d", getpid());
-	} 
-	
-	else 
-	{
-		//execve(command, parameters, 0);
-		printf("I am a child with PID: %d", getpid());
-	}
+		// read_command(command, parameters);
+
+		int childPid = fork();
+
+		if(childPid > 0) {
+			printf("Error!");
+			return 0;
+		}
+
+		if(childPid != 0) {
+			//waitpid(-1, &status, 0);
+			printf("I am a parent with PID: %d", getpid());
+		}
+		else {
+			//execve(command, parameters, 0);
+			printf("I am a child with PID: %d", getpid());
+		}
 	}
 	return 0;
 }
