422c0345472de5c0023f0b9557d205de6aad344d
Anders Schnack <s205445@dtu.dk>
2021-09-21 10:22:39 +0200

    Help-command, Anders Schnack, 10:22

---
+++ String/HelpCommand.c
@@ -0,0 +1,12 @@
+//
+// Created by Anders Schnack, 21-09-2021
+//
+
+#include "HelpCommand.h"
+#include <stdlib.h>
+#include <string.h>
+
+void Help() {
+    printf("\nThis is the help function");
+}
+
