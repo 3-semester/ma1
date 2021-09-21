b6876bb23fc9752f1d5224632af4296fe9e8ac10
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-09-21 10:17:14 +0200

    merge

---
+++ Shell/shell.c
@@ -0,0 +1,23 @@
+void shell_loop(void){
+	char** str;
+	int current_status;
+
+	while(current_status){
+		shell_read(str);
+        shell_execute(shell_parse(str));
+	}
+    
+    return 1;
+}
+
+void shell_read(char** args){
+
+}
+
+char** shell_parse(char** args){
+
+}
+
+void shell_execute(char** args){
+
+}
---
+++ Shell/shell.h
@@ -0,0 +1,38 @@
+#if !defined(MANDATORY_ASSIGNMENT_SHELL_H)
+#define MANDATORY_ASSIGNMENT_SHELL_H
+
+
+
+/**
+ * @brief Start the shell loop
+ *  Runs the shell loop until it gets cancelled.
+ */
+void shell_loop(void);
+
+
+/**
+ * @brief Read input from the command line and save it in args
+ * 
+ * @param args the variable to save arguments in
+ */
+void shell_read(char** args);
+
+/**
+ * @brief Parse the arguments 
+ * 
+ * @param args 
+ */
+char** shell_parse(char** args);
+
+
+/**
+ * @brief Execute the right program or inbuilt functions
+ * 
+ * @param args 
+ */
+void shell_execute(char** args);
+
+
+
+
+#endif // MANDATORY_ASSIGNMENT_SHELL_H
