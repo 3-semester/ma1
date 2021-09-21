85b6654aae3a8653226c8298611c01c7e1df4f92
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-09-21 23:01:49 +0200

    added arguments to hello world

--- programs/helloworld.c
+++ programs/helloworld.c
@@ -1,8 +1,22 @@
 
 #include <stdio.h>
 
+
+
+
 int main(int argc, char const *argv[])
 {
+
+    if((sizeof(argv) / sizeof((argv)[0])) > 0 && argv[1] != NULL){
+    printf("Hello %s!", argv[1]);
+    }else
+    {
     printf("Hello World!");
+    }
+    
+
+
+
+
     return 0;
 }
