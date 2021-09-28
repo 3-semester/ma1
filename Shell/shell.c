ebb8667eafe0b30f215f5805646d08e0236d2677
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-09-28 10:41:46 +0200

    ProposedAlternatives replaced originals

--- Shell/shell.c
+++ Shell/shell.c
@@ -31,40 +31,6 @@ void shell_loop(){
 	/*
 	 * Keep taking commands until the program is exited.
 	 */
-	char* userCommand = NULL;
-	while(current_status){
-		printf("Shell: ");
-		if (userCommand != NULL) free(userCommand);
-		userCommand = shell_read();
-
-		//check if userCommand contains pipe
-		if (!stringContainsCharacter(userCommand, '|')) {//if it doesnt contain pipe run single program
-			char **args = shell_parse(userCommand);
-			shell_execute(args);
-			freeStringArray(args);
-			continue;
-		}
-		//split up into two strings
-		char **argset1;
-		char **argset2;
-		shelldoubleparse(userCommand, &argset1, &argset2);
-
-		//print so you can see args
-		printf("\narg sets: #%s#%s# and #%s#%s#\n", argset1[0], argset1[1], argset2[0], argset2[1]);
-		fflush(stdout);
-
-		//execute
-		executeTwoProcesses(argset1, argset2);
-		freeStringArray(argset1);
-		freeStringArray(argset2);
-	}
-}
-
-
-void shell_loop_ProposedAlternative(){
-	/*
-	 * Keep taking commands until the program is exited.
-	 */
 	while(current_status){
 		printf("Shell: ");
 
@@ -87,11 +53,7 @@ char* shell_read(){
 	return trim(readLineFromStdin());
 }
 
-char** shell_parse(char* string){
-	return splitString(string, NULL);
-}
-
-char*** shell_parse_ProposedAlternative(char* userCommand){
+char*** shell_parse(char* userCommand){
 	char*** argss = calloc(500, sizeof(char**));
 
 	char** commands = (char***) splitString(userCommand, "|");
@@ -104,20 +66,7 @@ char*** shell_parse_ProposedAlternative(char* userCommand){
 	return argss;
 }
 
-void shell_execute(char** args){
-	pid_t pid = fork();
-	if(pid != 0){
-		waitpid(pid, &current_status, 0);
-		return;
-	}
-	//In child process from here on
-	if(execvp(args[0], args) == -1){
-		fprintf(stderr, "No command found called %s - ERRNO: %d", args[0], errno);
-	}
-	exit(0);
-}
-
-void shell_execute_ProposedAlternative(int numberOfArgs, char*** argss){
+void shell_execute(int numberOfArgs, char*** argss){
 	pid_t pid;
 	int* pipes = createPipes(numberOfArgs - 1);
 	for (int i = 0; i < numberOfArgs; ++i) {
