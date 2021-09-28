b331d297c665010b7ea20fe522da059435bffedb
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-09-28 10:51:07 +0200

    main.c no imports a bunch of things

--- main.c
+++ main.c
@@ -1,17 +1,11 @@
-#include <stdlib.h> 
-#include <stdio.h> 
-#include <unistd.h>
-#include <stdbool.h>
-#include <sys/wait.h>
 #include "Shell/shell.h"
-
 
 /**
  *	This is the first thing that runs when this program is started
  */
 int main(){
 	// spin up the shell
-	shell_loop_ProposedAlternative();
+	shell_loop();
 	return 0;
 }
 
