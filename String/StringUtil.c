d396a74a5a6a115031ecd32d41b570e0235860ff
Anders Schnack <s205445@dtu.dk>
2021-09-28 20:21:41 +0200

    deleted help-function

--- String/StringUtil.c
+++ String/StringUtil.c
@@ -12,6 +12,7 @@
 char* newString(unsigned long long length){
 	if (length > MAX_STRING_LENGTH) return NULL;
 
+
 	char* string = NULL;
 	while (string == NULL) string = calloc(length + 1, sizeof(char));
 	return string;
